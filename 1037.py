83a6270c6079d8e50b77f0dfa1cea27c1e241625
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2018-12-08 12:43:51 +0800

    Create 1037.py

---
+++ 1037.py
@@ -0,0 +1,12 @@
+a,b = input().split()
+a = [int(i) for i in a.split('.')]
+b = [int(i) for i in b.split('.')]
+out = b[0]*17*29+b[1]*29+b[2] - a[0]*17*29+a[1]*29+a[2]
+
+if out<0:
+    out = -out
+    print('-',end="")
+s = out//(17*29)
+g = (out-s*17*29)//29
+k = (out-s*17*29-g*29)
+print(str(s)+'.'+str(g)+'.'+str(k))
