3aa493e58479304eda01bdc310b33e2ab06d7610
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2019-02-24 23:46:32 +0800

    Create 1087.cpp

---
+++ 1087.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    int n;
+    cin>>n;
+    set<int>s;
+    for(int i = 1;i <= n;i++)
+    {
+        s.insert(i/2 + i/3 + i/5);
+    }
+    cout<<s.size()<<endl;
+    return 0;
+}
